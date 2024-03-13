c3044a18aa0f2504ce76d7be616ed672d4094a36
su1gen <prybylovychav@gmail.com>
2024-03-13 18:26:06 +0200

    Update

--- lib/upgraded_reviewables/posts_controller_extension.rb
+++ lib/upgraded_reviewables/posts_controller_extension.rb
@@ -25,17 +25,17 @@ module UpgradedReviewables
 
         if iaJson["action"].to_s == "create_post"
           topic = Topic.find_by_id(iaJson[:post][:topic_id])
-          if topic.private_message?
-#             topic.allowed_users.each do |au|
-#               if au.username != "AIAssistant" && au.username != "Mediator" && au.id != current_user["id"]
-#                 MessageBus.publish("/user-messages/#{au.id}", {
-#                   action: 'show_new_private_message',
-#                   topic_id: iaJson[:post][:topic_id],
-#                   post_id: iaJson[:post][:id],
-#                   post_url: "/t/#{iaJson[:post][:topic_slug]}/#{iaJson[:post][:topic_id]}/#{iaJson[:post][:post_number]}",
-#                 })
-#               end
-#             end
+          if !topic.nil? && topic.private_message?
+            topic.allowed_users.each do |au|
+              if au.username != "AIAssistant" && au.username != "Mediator" && au.id != current_user["id"]
+                MessageBus.publish("/user-messages/#{au.id}", {
+                  action: 'show_new_private_message',
+                  topic_id: iaJson[:post][:topic_id],
+                  post_id: iaJson[:post][:id],
+                  post_url: "/t/#{iaJson[:post][:topic_slug]}/#{iaJson[:post][:topic_id]}/#{iaJson[:post][:post_number]}",
+                })
+              end
+            end
           end
         end
 
